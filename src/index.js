324f23a5e0f2bf19062f916365787d367dde178f
NovicovMykhailo <mikenovicov@gmail.com>
2023-03-28 18:44:33 +0300

    require tasks

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+// const Handlebars = require('handlebars');
+
+// new SimpleLightbox('.some-element a', {
+//   /* options */
+// });
+
+// Notiflix.Notify.success('Sol lucet omnibus');
