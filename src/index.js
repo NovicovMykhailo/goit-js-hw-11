1e3f658ac81cce6df8317587403e82611e87102e
NovicovMykhailo <mikenovicov@gmail.com>
2023-03-30 15:23:42 +0300

    updj

--- src/index.js
+++ src/index.js
@@ -19,21 +19,27 @@ const refs = {
   gallery: document.querySelector('.gallery'),
   form: document.querySelector('.search-form'),
   spinner: document.querySelector('.lds-dual-ring '),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
 const inputEl = refs.form.elements[0];
-const submitBtn = refs.form.elements[1];
 
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
   refs.spinner.style.opacity = 1;
   try {
-    getImages(inputEl.value).then(() => {
-      refs.spinner.style.opacity = 0;
-    });
+    if (inputEl.value !== '') {
+      getImages(inputEl.value).then(() => {
+        refs.spinner.style.opacity = 0;
+      });
+    } else {
+      Notiflix.Notify.warning(`The search tab cannot be empty`);
+      clearAll();
+    }
   } catch {
     refs.spinner.style.opacity = 0;
-    Notiflix.Notify.error('Sol lucet omnibus');
+    Notiflix.Notify.failure('Ooops, Something went wrong');
+    refs.loadMoreBtn.style.display = 'none';
   }
 });
 
@@ -42,19 +48,35 @@ async function getImages(query) {
     .fetch(query)
     .then(data => {
       if (data.totalHits !== 0) {
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        console.log(data);
+        Notiflix.Notify.success(`Hooray! We found ${data.total} images.`);
+      } else if (pixabayApi.page === data.totalHits) {
+        Notiflix.Notify.info(
+          `We're sorry, but you've reached the end of search results.`
+        );
+        refs.loadMoreBtn.style.display = 'none';
       } else {
         Notiflix.Notify.info(
           `Sorry! We can't find any images. at your request. Please try again`
         );
+        clearAll();
+        return;
       }
 
       return data.hits;
     })
     .then(images => {
+      if (images.length < 9) {
+        console.log(images.length);
+        refs.loadMoreBtn.style.display = 'none';
+      } else {
+        refs.loadMoreBtn.style.display = 'block';
+      }
       const markup = images.map(createMarkup);
       refs.gallery.innerHTML = markup.join('');
+
       addSimpleLightBox();
+      loadMoreImages(query);
     });
 }
 
@@ -67,12 +89,59 @@ function addSimpleLightBox() {
     preloading: true,
     animationSpeed: 0,
     animationSlide: true,
-    showCounter: false,
+    // showCounter: false,
   };
 
   let gallery = new SimpleLightbox('.gallery a', options);
   gallery.on('show.simplelightbox');
+  return gallery;
+}
+function updateSimpleLightBox() {
+  const options = {
+    enableKeyboard: true,
+    docClose: true,
+    doubleTapZoom: 2,
+    scrollZoom: true,
+    preloading: true,
+    animationSpeed: 0,
+    animationSlide: true,
+    // showCounter: false,
+  };
+
+  let gallery = new SimpleLightbox('.gallery a', options);
+  gallery.refresh();
+}
+
+function loadMoreImages(query) {
+  refs.loadMoreBtn.addEventListener('click', () => {
+    pixabayApi.page += 1;
+    pixabayApi.fetch(query).then(data => {
+      console.log(data);  
+      refs.gallery.insertAdjacentHTML(
+        'beforeEnd',
+        data.hits.map(createMarkup).join('')
+        // updateSimpleLightBox()
+      );
+    });
+  });
 }
 
 // SimpleLightBox Бібліотека містить метод refresh(),
 // який обов'язково потрібно викликати щоразу після додавання нової групи карток зображень.
+function clearAll() {
+  refs.spinner.style.opacity = 0;
+  refs.loadMoreBtn.style.display = 'none';
+  refs.gallery.innerHTML = '';
+}
+
+// Зробити плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень.
+//  Ось тобі код - підказка, але розберися у ньому самостійно.
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
